6d2fb0f223e8cc23a5fe161583228fa53969fd36
MaTtiaa01 <mattia2001@live.it>
2022-10-11 12:40:06 +0100

    add gioco dei dati structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-alert("it works");
+//Mail
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+//Gioco dei dadi
+//Generare un numero random da 1 a 6
+//sia per il giocatore 
+const playerNumber = Math.floor((Math.random() * 5) + 1).toFixed(0);
+console.log(playerNumber);
+
+//sia per il computer. 
+const computerNumber = Math.floor((Math.random() * 5) + 1).toFixed(0);
+console.log(computerNumber);
+
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+if (playerNumber > computerNumber){
+    //il giocatore ha vinto
+} else if (playerNumber = computerNumber){
+    //nessuno ha vinto parità
+} else{
+    //il computer ha vinto
+}
